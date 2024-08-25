d752f7fe1c60500cf329a029ad4bc8fb2609ffc8
Alexis Drai <alexis@alexisdrai.com>
2024-08-25 22:41:33 +0200

    Audit fix, reformat

--- routes/api.js
+++ routes/api.js
@@ -2,17 +2,15 @@
 
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
-module.exports = function (app) {
-  
+module.exports = function(app) {
+
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route('/api/check').post((req, res) => {
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+  });
 
-    });
+  app.route('/api/solve').post((req, res) => {
+
+  });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,4 +1,4 @@
-const chai = require("chai");
+const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = chai.assert;
 const server = require('../server');
